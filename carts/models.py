081a90134710c603be6b0f00d324c1947f9c6c82
Khan2406 <82305740+khan215701@users.noreply.github.com>
2021-11-26 15:29:39 +0530

    cart funcationality and calculate amount

--- carts/models.py
+++ carts/models.py
@@ -4,7 +4,7 @@ from store.models import Product
 # Create your models here.
 
 
-class cart(models.Model):
+class Cart(models.Model):
     cart_id = models.CharField(max_length=200, blank=True)
     date_added = models.DateTimeField(auto_now_add=True)
 
@@ -12,11 +12,14 @@ class cart(models.Model):
         return self.cart_id
 
 
-class cart_item(models.Model):
+class CartItem(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    cart = models.ForeignKey(cart, on_delete=models.CASCADE)
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
     quantity = models.IntegerField()
     is_active = models.BooleanField(default=True)
+
+    def sub_total(self):
+        return self.quantity * self.product.price
 
     def __str__(self):
         return self.product
